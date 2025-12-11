9e6113e6e522be9b85346dba7945085f9f0ab602
wuquanlong <wuquanlong@licaimofang.com>
2025-12-11 22:21:55 +0800

    fix:修改返回数据结构和时间格式

--- lib/routes/twitter/tweet.ts
+++ lib/routes/twitter/tweet.ts
@@ -3,6 +3,7 @@ import api from './api';
 import utils from './utils';
 import { fallback, queryToBoolean } from '@/utils/readable-social';
 import { config } from '@/config';
+import logger from "@/utils/logger";
 
 export const route: Route = {
     path: '/tweet/:id/status/:status/:original?',
@@ -52,16 +53,42 @@ async function handler(ctx) {
     };
     await api.init();
     const userInfo = await api.getUser(id);
-    const data = await api.getUserTweet(id, params);
+
     const profileImageUrl = userInfo.profile_image_url || userInfo.profile_image_url_https;
     //const item = original && config.isPackage ? data : utils.ProcessFeed(ctx, { data });
-    const item = data;
+    let data;
+    try {
+        data = await  api.getUserTweetsAndReplies(id, params);
+        if (!include_rts) {
+            data = utils.excludeRetweet(data);
+        }
+    } catch (error) {
+        logger.error(error);
+    }
+    console.log('返回数据量:', data?.length || 0);
+
+    // 移除 user 字段
+    const dataWithoutUser = removeUserFromTweets(data);
+
+    // 处理 RSS 项目
+    const feedItems = data ? utils.ProcessFeed(ctx, { data }) : [];
+
+    // 添加处理后的数据到描述中
+    if (feedItems.length > 0 && dataWithoutUser) {
+        feedItems.forEach((item, index) => {
+            let tweetData = dataWithoutUser[index];
+
+            if (tweetData) {
+                item.description = tweetData;
+            }
+        });
+    }
 
     return {
         title: `Twitter @${userInfo.name}`,
         link: `https://x.com/${userInfo.screen_name}/status/${status}`,
         image: profileImageUrl.replace(/_normal.jpg$/, '.jpg'),
         description: userInfo.description,
-        item,
+        item: feedItems,
     };
 }
