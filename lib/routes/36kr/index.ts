ecd7f0122b749be3d6a64c1527c40c75905d7a66
wuquanlong <wuquanlong@licaimofang.com>
2025-12-22 13:19:25 +0800

    add:36kr

--- lib/routes/36kr/index.ts
+++ lib/routes/36kr/index.ts
@@ -43,23 +43,23 @@ async function handler(ctx) {
     const response = await got({
         method: 'get',
         url: currentUrl,
-        headers: {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36',
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-            'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
-            'Accept-Encoding': 'gzip, deflate, br',
-            'Connection': 'keep-alive',
-            'Upgrade-Insecure-Requests': '1',
-            'Cache-Control': 'max-age=0',
-            'sec-ch-ua': '"Chromium";v="94", "Google Chrome";v="94", ";Not A Brand";v="99"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"Windows"',
-            'Sec-Fetch-Dest': 'document',
-            'Sec-Fetch-Mode': 'navigate',
-            'Sec-Fetch-Site': 'none',
-            'Sec-Fetch-User': '?1',
-            'DNT': '1'
-        },
+        // headers: {
+        //     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36',
+        //     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        //     'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
+        //     'Accept-Encoding': 'gzip, deflate, br',
+        //     'Connection': 'keep-alive',
+        //     'Upgrade-Insecure-Requests': '1',
+        //     'Cache-Control': 'max-age=0',
+        //     'sec-ch-ua': '"Chromium";v="94", "Google Chrome";v="94", ";Not A Brand";v="99"',
+        //     'sec-ch-ua-mobile': '?0',
+        //     'sec-ch-ua-platform': '"Windows"',
+        //     'Sec-Fetch-Dest': 'document',
+        //     'Sec-Fetch-Mode': 'navigate',
+        //     'Sec-Fetch-Site': 'none',
+        //     'Sec-Fetch-User': '?1',
+        //     'DNT': '1'
+        // },
         timeout: 30000,
         retry: 2
     });
